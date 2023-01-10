edf8c64280ba83c2a7adfcd3b7f7136982296eeb
Leo Ardon <leo.ardon.la@gmail.com>
2023-01-10 19:38:13 +0000

    clean up

--- run_manual.py
+++ run_manual.py
@@ -6,66 +6,34 @@ from rm_marl.trainer import Trainer
 import gym
 import os
 
+AGENT_RANDOM_LABEL_CONFIG = {
+    1: {"br": (0.3, ButtonsEnv.open_walls_R)},
+    2: {"by": (0.3, ButtonsEnv.open_walls_Y), "br": (0.3, ButtonsEnv.open_walls_R)},
+    3: {"bg": (0.3, ButtonsEnv.open_walls_G), "br": (0.3, ButtonsEnv.open_walls_R)}
+}
 
-def create_env1():
+
+def _create_env(agent_id):
     env = gym.make(
         "rm-marl/Buttons-v0",
         render_mode="rgb_array",
         file="/Users/leo/dev/phd/rm-marl/data/buttons/env.txt",
     )
-    # env = LabelingFunctionWrapper(env)
-    env = SingleAgentEnvWrapper(env, "A1")
+    env = SingleAgentEnvWrapper(env, f"A{agent_id}")
     env = ButtonsLabelingFunctionWrapper(env)
-    env = RandomLabelingFunctionWrapper(env, {"br": (0.3, ButtonsEnv.open_walls_R)})
+    env = RandomLabelingFunctionWrapper(env, AGENT_RANDOM_LABEL_CONFIG[agent_id])
     env = RewardMachineWrapper(
         env,
         RewardMachine.load_from_file(
-            "/Users/leo/dev/phd/rm-marl/data/buttons/rm_agent_1.txt"
+            f"data/buttons/rm_agent_{agent_id}.txt"
         ),
         label_mode=RewardMachineWrapper.LabelMode.STATE,
     )
     env = RecordEpisodeStatistics(env)
     return env
 
-def create_env2():
-    env = gym.make(
-        "rm-marl/Buttons-v0",
-        render_mode="rgb_array",
-        file="/Users/leo/dev/phd/rm-marl/data/buttons/env.txt",
-    )
-    # env = LabelingFunctionWrapper(env)
-    env = SingleAgentEnvWrapper(env, "A2")
-    env = ButtonsLabelingFunctionWrapper(env)
-    env = RandomLabelingFunctionWrapper(env, {"by": (0.3, ButtonsEnv.open_walls_Y), "br": (0.3, ButtonsEnv.open_walls_R)})
-    env = RewardMachineWrapper(
-        env,
-        RewardMachine.load_from_file(
-            "/Users/leo/dev/phd/rm-marl/data/buttons/rm_agent_2.txt"
-        ),
-        label_mode=RewardMachineWrapper.LabelMode.STATE,
-    )
-    env = RecordEpisodeStatistics(env)
-    return env
-
-def create_env3():
-    env = gym.make(
-        "rm-marl/Buttons-v0",
-        render_mode="rgb_array",
-        file="/Users/leo/dev/phd/rm-marl/data/buttons/env.txt",
-    )
-    # env = LabelingFunctionWrapper(env)
-    env = SingleAgentEnvWrapper(env, "A3")
-    env = ButtonsLabelingFunctionWrapper(env)
-    env = RandomLabelingFunctionWrapper(env, {"bg": (0.3, ButtonsEnv.open_walls_G), "br": (0.3, ButtonsEnv.open_walls_R)})
-    env = RewardMachineWrapper(
-        env,
-        RewardMachine.load_from_file(
-            "/Users/leo/dev/phd/rm-marl/data/buttons/rm_agent_3.txt"
-        ),
-        label_mode=RewardMachineWrapper.LabelMode.STATE,
-    )
-    env = RecordEpisodeStatistics(env)
-    return env
+def create_local_envs():
+    return {f"E{aid}": _create_env(aid) for aid in AGENT_RANDOM_LABEL_CONFIG.keys()}
 
 def create_shared_env():
     env = gym.make(
@@ -84,14 +52,7 @@ def create_shared_env():
     env = RecordEpisodeStatistics(env)
     return {"G": env}
 
-def create_envs():
-    return {
-        "E1": create_env1(),
-        "E2": create_env2(),
-        # "E3": create_env3(),
-    }
-
-def create_agents(envs):
+def create_rm_agents(envs):
     return {
         "A1": RewardMachineAgent(
             RewardMachine.load_from_file(
@@ -119,7 +80,7 @@ def create_agents(envs):
         )
     }
 
-def create_learning_agents(envs):
+def create_rm_learning_agents(envs):
     return {
         "A1": RewardMachineLearningAgent(
             algo_kws={
@@ -142,14 +103,16 @@ def create_learning_agents(envs):
 
 if __name__ == "__main__":
 
-    # envs = create_envs()
-    # # agents = create_agents(envs)
-    # agents = create_learning_agents(envs)
+    # Training
+    # envs = create_local_envs()
+    
+    # # agents = create_rm_agents(envs)
+    # agents = create_rm_learning_agents(envs)
 
     # trainer = Trainer(envs, agents)
 
     # trainer.run({
-    #     "experiment": "buttons_single_agent",
+    #     "experiment": "buttons",
     #     "training": True,
     #     "total_episodes": 10000,
     #     "log_dir": os.path.join(os.path.dirname(__file__), "logs"),
@@ -157,6 +120,29 @@ if __name__ == "__main__":
     #     "recording_freq": 500,
     #     "seed": 123
     # })
+
+##########################################################
+
+    # Evaluation
+    # path = "logs/buttons_single_agent/2023-01-01_16-26-25/trainer.pkl"
+    # trainer_load = Trainer.load(path)
+    # agents = trainer_load.agents
+    
+    # envs = create_shared_env()
+
+    # trainer = Trainer(envs, agents)
+
+    # trainer.run({
+    #     "experiment": "buttons_single_agent_test",
+    #     "training": False,
+    #     "greedy": False,
+    #     "total_episodes": 1,
+    #     "log_dir": os.path.join(os.path.dirname(__file__), "logs"),
+    #     "log_freq": 1,
+    #     "recording_freq": 1,
+    #     "seed": 10000 + 123
+    # })
+
 
 ########################################################################################
 
@@ -174,27 +160,10 @@ if __name__ == "__main__":
 
     while not done:
         env.render()
-        a = input("action: ")
+        a = input("action for A1: ")
         obs, reward, terminated, truncated, info = env.step({"A1": int(a)})
         done = terminated or truncated
         print(info)
 
 ############################################
-    # path = "/Users/leo/dev/phd/rm-marl/logs/buttons_single_agent/2023-01-01_16-26-25/trainer.pkl"
-    # trainer_load = Trainer.load(path)
-    # envs = create_shared_env()
-    # # envs = {"E3": create_env3()}
-    # agents = trainer_load.agents
-
-    # trainer = Trainer(envs, agents)
-
-    # trainer.run({
-    #     "experiment": "buttons_single_agent_test",
-    #     "training": False,
-    #     "greedy": False,
-    #     "total_episodes": 1,
-    #     "log_dir": os.path.join(os.path.dirname(__file__), "logs"),
-    #     "log_freq": 1,
-    #     "recording_freq": 1,
-    #     "seed": 10000 + 123
-    # })
+    
