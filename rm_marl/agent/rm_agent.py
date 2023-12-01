eae27ab63bf2ec03172e28dcbfea9a98a05e5c3f
Leo Ardon <leo.ardon.la@gmail.com>
2023-12-01 16:52:56 +0000

    extend agent.action interface

--- rm_marl/agent/rm_agent.py
+++ rm_marl/agent/rm_agent.py
@@ -34,8 +34,8 @@ class RewardMachineAgent:
     def reset(self, seed: Optional[int] = None):
         self.u = self.rm.u0
 
-    def action(self, state, greedy: bool = False):
-        return self.algo.action(state, self.u, greedy=greedy)
+    def action(self, state, greedy: bool = False, **algo_args):
+        return self.algo.action(state, self.u, greedy=greedy, **algo_args)
 
     def learn(self, state, u, action, reward, done, next_state, next_u):
         return self.algo.learn(state, u, action, reward, done, next_state, next_u)
