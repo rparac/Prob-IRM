9474c32d4511c8c81af19bce5680940bb0277a83
Leo Ardon <leo.ardon.la@gmail.com>
2023-12-01 17:02:54 +0000

    separation between AutomatWrapper and RewardMachineWrapper

--- rm_marl/envs/wrappers.py
+++ rm_marl/envs/wrappers.py
@@ -1,16 +1,13 @@
 import abc
 import copy
 from dataclasses import dataclass
-import os
 from enum import Enum
-from itertools import groupby
 from typing import TYPE_CHECKING
 
 import gym
 from gym.wrappers import RecordEpisodeStatistics, TimeLimit
 
-if TYPE_CHECKING:
-    from ..reward_machine import RewardMachine
+from ..reward_machine import RewardMachine
 
 
 class NumberStepsDiscountedRewardWrapper(gym.Wrapper):
@@ -72,6 +69,7 @@ class RandomLabelingFunctionWrapper(gym.Wrapper):
         return tuple(e for es in self.trace for e in es)
 
     def get_labels(self, _obs: dict = None, _prev_obs: dict = None):
+        # Generate one random event at a time
         valid_events = [e for e, c in self.random_events.items() if c.condition(self)]
         if not valid_events:
             return []
@@ -82,15 +80,23 @@ class RandomLabelingFunctionWrapper(gym.Wrapper):
             return [event]
 
         return []
+    
+        # Generate multiple random event at a time
+        # valid_events = [
+        #     e 
+        #     for e, c in self.random_events.items() 
+        #     if c.condition(self) and self.np_random.random() <= c.proba
+        # ]
+        # return valid_events
 
     def step(self, action):
         observation, reward, terminated, truncated, info = super().step(action)
         labels, simulated_env_updates = info.get("labels", []), {}
-        # two events can not happen at the same time
-        if not labels:
-            labels = self.get_labels()
-            for l in labels:
-                simulated_env_updates[l] = self.random_events[l].env_update
+
+        random_labels = self.get_labels()
+        for l in random_labels:
+            simulated_env_updates[l] = self.random_events[l].env_update
+            labels.append(l)
         
         self.trace.append(labels or [])
         info["labels"] = labels
@@ -106,24 +112,36 @@ class RandomLabelingFunctionWrapper(gym.Wrapper):
         return True
 
 
-class RewardMachineWrapper(gym.Wrapper):
+class AutomataWrapper(gym.Wrapper):
     class LabelMode(Enum):
         ALL = 0
         RM = 1
         STATE = 2
 
+    class TerminationMode(Enum):
+        RM = 0
+        ENV = 1
+
     def __init__(
-        self, env: gym.Env, rm: "RewardMachine", label_mode: LabelMode = LabelMode.ALL
+        self, 
+        env: gym.Env, 
+        rm: "RewardMachine", 
+        label_mode: LabelMode = LabelMode.ALL,
+        termination_mode: TerminationMode = TerminationMode.RM
     ):
         """
         label_mode:
             - ALL: returns all the labels returned by the labeling function
             - RM: returns only the labels present in the RM
             - STATE: returns only the lables that can be observed from the current state
+        termination_mode:
+            - RM: ends the episode when the RM reaches accepting/rejecting state
+            - ENV: ends the episode when the underlying env is returning the end signal
         """
         assert hasattr(env, "get_labels"), "The LabelingFunctionWrapper is required"
 
         self.label_mode = label_mode
+        self.termination_mode = termination_mode
 
         self.rm = rm
         self.u = self.rm.u0
@@ -141,37 +159,86 @@ class RewardMachineWrapper(gym.Wrapper):
             return event in self.rm.get_valid_events(u)
 
     def step(self, action):
-        observation, _reward, _terminated, truncated, info = super().step(action)
+        observation, reward, terminated, truncated, info = super().step(action)
         info["labels"] = self.filter_labels(info["labels"], self.u)
         simulated_updates = info.pop("env_simulated_updates", {})
 
-        reward = 0
-        for e in info["labels"]:
-            # apply simulated updates to the environment
-            if e in simulated_updates:
-                simulated_updates[e](self.unwrapped)
+        info["labels"] = self._apply_simulated_updates(info["labels"], simulated_updates)
         
         u_next = self.rm.get_next_state(self.u, info["labels"])
-        reward += self.rm.get_reward(self.u, u_next)
+        reward = self._get_reward(reward, u_next)
         self.u = u_next
 
-        terminated = self.rm.is_state_terminal(self.u)
+        terminated = self._get_terminated(terminated)
         info["rm_state"] = self.u
 
         return observation, reward, terminated, truncated, info
 
+    def _get_reward(self, reward, u_next):
+        return reward
+    
+    def _get_terminated(self, terminated):
+        if self.termination_mode == self.TerminationMode.ENV:
+            return terminated
+        else: # should be TerminationMode.RM
+            return self.rm.is_state_terminal(self.u)
+
+    def _apply_simulated_updates(self, original_labels, simulated_updates):
+        labels = copy.deepcopy(original_labels)
+        for e in original_labels:
+            # apply simulated updates to the environment
+            if e in simulated_updates:
+                labels_update = simulated_updates[e](self.unwrapped)
+                if labels_update:
+                    labels = labels_update(labels)
+        return labels
+
     def reset(self, **kwargs):
         obs, info = super().reset(**kwargs)
         self.u = self.rm.u0
+
+        info["labels"] = self.filter_labels(info.get("labels", {}), self.u)
+        simulated_updates = info.pop("env_simulated_updates", {})
+        info["labels"] = self._apply_simulated_updates(info["labels"], simulated_updates)
+        
+        u_next = self.rm.get_next_state(self.u, info["labels"])
+        self.u = u_next
+
         info["rm_state"] = self.u
         return obs, info
 
+class RewardMachineWrapper(AutomataWrapper):
+
+    def __init__(
+            self, 
+            env: gym.Env, 
+            rm: RewardMachine, 
+            label_mode: AutomataWrapper.LabelMode = AutomataWrapper.LabelMode.ALL, 
+            termination_mode: AutomataWrapper.TerminationMode = AutomataWrapper.TerminationMode.RM,
+            reward_function: callable = None
+    ):
+        super().__init__(env, rm, label_mode, termination_mode)
+
+        self.reward_function = reward_function or self._simple_reward_func
+
+    @staticmethod
+    def _simple_reward_func(rm, u, u_next, reward):
+        return rm.get_reward(u, u_next)
+
+    def _get_reward(self, reward, u_next):
+        return self.reward_function(self.rm, self.u, u_next, reward)
 
 class SingleAgentEnvWrapper(gym.Wrapper):
     def __init__(self, env: gym.Env, agent_id: str):
         self.agent_id = agent_id
 
         super().__init__(env)
+
+        self.observation_space = gym.spaces.Dict({
+            aid: obs_space
+            for aid, obs_space in self.observation_space.spaces.items()
+            if aid == self.agent_id
+        })
 
     def reset(self, **kwargs):
         obs, info = super().reset(**kwargs)
