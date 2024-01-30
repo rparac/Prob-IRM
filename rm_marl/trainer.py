39ec04f6b5d46b6a1c1b4e521e8fdc28833466f2
Roko Parac <rp218@ic.ac.uk>
2024-01-30 10:06:03 +0100

    Fix pickling of lambda

--- rm_marl/trainer.py
+++ rm_marl/trainer.py
@@ -27,7 +27,7 @@ class Trainer:
         # Logs for plots more that SummaryWriter can't represent, so we use matplotlib + SummaryWritter add image
 
         # Keeps track of all rm states in each environment. Used for logging of state transition diagram.
-        self.all_recorded_rm_states: Dict[str, set] = defaultdict(lambda: set())
+        self.all_recorded_rm_states: Dict[str, set] = defaultdict(_generate_empty_set)
         # Stores for each episode in an environment a dictionary of (RM state, timestep) pairs
         # from env_id -> [{u -> last_timestep}]
         self.last_timestep_train_info: Dict[str, List[Dict[str, int]]] = {}
@@ -56,7 +56,8 @@ class Trainer:
 
         if run_config["extra_debug_information"]:
             create_rm_state_logs(log_dir, run_config["total_episodes"], self.test_episodes,
-                                 run_config["testing_freq"], self.last_timestep_train_info, self.last_timestep_test_info,
+                                 run_config["testing_freq"], self.last_timestep_train_info,
+                                 self.last_timestep_test_info,
                                  self.all_recorded_rm_states, self.rm_relearned_episodes)
 
         _ = [e.close() for e in self.envs.values()]
@@ -307,3 +308,8 @@ class Trainer:
         if "trainer.pkl" not in path:
             path = os.path.join(path, "trainer.pkl")
         return joblib.load(path)
+
+
+# Lambda cannot be used because of a pickling issue
+def _generate_empty_set():
+    return set()
