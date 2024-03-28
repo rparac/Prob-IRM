9d3cd15f8cc12b27f22b0000ec02457c8c35d892
Roko Parac <roko.parac@gmail.com>
2024-03-28 12:08:15 +0100

    Tests visit abcd with daniel

--- daniel.py
+++ daniel.py
@@ -1,20 +1,25 @@
 import gym
 
 from rm_marl.envs.gym_subgoal_automata_wrapper import GymSubgoalAutomataAdapter, \
-    OfficeWorldDeliverCoffeeLabelingFunctionWrapper, OfficeWorldOfficeLabelingFunctionWrapper, \
-    OfficeWorldPlantLabelingFunctionWrapper, OfficeWorldCoffeeLabelingFunctionWrapper, \
-    OfficeWorldMailLabelingFunctionWrapper
+    OfficeWorldBLabelingFunctionWrapper, OfficeWorldCLabelingFunctionWrapper, OfficeWorldDLabelingFunctionWrapper, \
+    OfficeWorldALabelingFunctionWrapper
 from rm_marl.envs.wrappers import NoisyLabelingFunctionComposer
 
-env = gym.make("gym_subgoal_automata:OfficeWorldDeliverCoffeeAndMail-v0",
+env = gym.make("gym_subgoal_automata:OfficeWorldPatrolABCD-v0",
                params={"generation": "random", "environment_seed": 0, "hide_state_variables": True})
 env = GymSubgoalAutomataAdapter(env, render_mode="human", agent_id="A1")  # type: ignore
 # env = OfficeWorldDeliverCoffeeLabelingFunctionWrapper(env)
-office_l = OfficeWorldOfficeLabelingFunctionWrapper(env, sensor_true_confidence=1, sensor_false_confidence=1)
-plant_l = OfficeWorldPlantLabelingFunctionWrapper(env, sensor_true_confidence=0.9, sensor_false_confidence=0.9)
-coffee_l = OfficeWorldCoffeeLabelingFunctionWrapper(env, sensor_true_confidence=1, sensor_false_confidence=1)
-mail_l = OfficeWorldMailLabelingFunctionWrapper(env, sensor_true_confidence=1, sensor_false_confidence=1)
-env = NoisyLabelingFunctionComposer([office_l, plant_l, coffee_l, mail_l])
+# office_l = OfficeWorldOfficeLabelingFunctionWrapper(env, sensor_true_confidence=1, sensor_false_confidence=1)
+# plant_l = OfficeWorldPlantLabelingFunctionWrapper(env, sensor_true_confidence=0.9, sensor_false_confidence=0.9)
+# coffee_l = OfficeWorldCoffeeLabelingFunctionWrapper(env, sensor_true_confidence=1, sensor_false_confidence=1)
+# mail_l = OfficeWorldMailLabelingFunctionWrapper(env, sensor_true_confidence=1, sensor_false_confidence=1)
+# env = NoisyLabelingFunctionComposer([office_l, plant_l, coffee_l, mail_l])
+a_l = OfficeWorldALabelingFunctionWrapper(env, sensor_true_confidence=1, sensor_false_confidence=0.99)
+b_l = OfficeWorldBLabelingFunctionWrapper(env, sensor_true_confidence=1, sensor_false_confidence=0.99)
+c_l = OfficeWorldCLabelingFunctionWrapper(env, sensor_true_confidence=1, sensor_false_confidence=0.99)
+d_l = OfficeWorldDLabelingFunctionWrapper(env, sensor_true_confidence=1, sensor_false_confidence=0.99)
+env = NoisyLabelingFunctionComposer([a_l, b_l, c_l, d_l])
+# env = NoisyLabelingFunctionComposer([office_l, plant_l, coffee_l, mail_l])
 
 key_to_act = {
     "w": 0,
@@ -29,6 +34,6 @@ while True:
 
     action = {"A1": key_to_act[x]}
     obs, reward, terminated, truncated, info = env.step(action)
-    # print(env.get_labels(obs, prev_obs=None))
+    print(env.get_labels(obs, prev_obs=None))
     if terminated:
         print(reward)
