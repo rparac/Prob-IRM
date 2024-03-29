7bb71cff86c3b6e5a0a7a0e9732164ced214bd31
Roko Parac <roko.parac@gmail.com>
2024-03-29 11:18:31 +0100

    Extracts more configuration into hydra

--- dqrm_coffee_world.py
+++ dqrm_coffee_world.py
@@ -6,7 +6,7 @@ import hydra
 import numpy as np
 from gym.wrappers import RecordEpisodeStatistics
 from hydra.utils import instantiate
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
 from rm_marl.agent import RewardMachineAgent, RewardMachineLearningAgent
 from rm_marl.algo import QRM
@@ -19,13 +19,14 @@ from rm_marl.rm_transition.prob_rm_transitioner import ProbRMTransitioner
 from rm_marl.trainer import Trainer
 
 
-def _get_base_env(env_name, seed, agent_id, label_factories):
+def _get_base_env(env_name, seed, agent_id, label_factories, render_mode, max_episode_length, use_rs):
     rm_transitioner = ProbRMTransitioner(rm=RewardMachineAgent.default_rm())
 
     # env=gym.make(
     env = gym.make(env_name,
-            params={"generation": "random", "environment_seed": seed, "hide_state_variables": True})
-    env = GymSubgoalAutomataAdapter(env, agent_id, render_mode="rgb_array", max_episode_length=250)  # type: ignore
+                   params={"generation": "random", "environment_seed": seed, "hide_state_variables": True})
+    env = GymSubgoalAutomataAdapter(env, agent_id, render_mode=render_mode,  # type: ignore
+                                    max_episode_length=max_episode_length)
     labeling_funs = []
     for label_factory in label_factories:
         labeling_funs.append(label_factory(env))
@@ -40,7 +41,8 @@ def _get_base_env(env_name, seed, agent_id, label_factories):
         termination_mode=AutomataWrapper.TerminationMode.ENV,
     )
 
-    env = ProbabilisticRewardShaping(env, shaping_rm=rm_transitioner.rm, discount_factor=0.99)
+    if use_rs:
+        env = ProbabilisticRewardShaping(env, shaping_rm=rm_transitioner.rm, discount_factor=0.99)
     env = RecordEpisodeStatistics(env)  # type: ignore
 
     return env
@@ -74,14 +76,13 @@ def run(cfg: DictConfig) -> int:
     env_config = cfg["env"]
     label_factories = [instantiate(label_factory_conf) for label_factory_conf in env_config["label_factories"]]
 
-    agent_config = env_config["agent"]
-
+    print(env_config)
     envs = []
     rm_agents = []
-    num_envs = 10
-    for i in range(num_envs):
+    for i in range(run_config["num_envs"]):
         agent_id = f"A{i + 1}"
-        env = _get_base_env(env_config["name"], run_config["seed"] + i, agent_id, label_factories)
+        env = _get_base_env(env_config["name"], run_config["seed"] + i, agent_id, label_factories,
+                            env_config["render_mode"], env_config["max_episode_length"], run_config["use_rs"])
         envs.append(env)
         # algo = DeepQRM(
         #     action_space=env.action_space,
@@ -97,12 +98,8 @@ def run(cfg: DictConfig) -> int:
 
         algo = QRM(**{
             "action_space": env.action_space,
-            "seed": 123,
-            "epsilon_start": 1.0,
-            "epsilon_end": 0.1,
-            "epsilon_decay": 50,
-            "gamma": 0.99,
-            "alpha": 0.1,
+            "seed": run_config["seed"],
+            **cfg["algo"],
         })
         rm_agent = RewardMachineAgent.default_rm_agent(env.agent_id, env.rm_transitioner, algo)
         rm_agents.append(rm_agent)
@@ -110,18 +107,16 @@ def run(cfg: DictConfig) -> int:
     learning_ag = RewardMachineLearningAgent(
         rm_agent=rm_agents,
         rm_learner_cls=ProbFFNSLLearner,
-        rm_learner_kws={
-            "edge_cost": run_config["edge_cost"],
-            "n_phi_cost": run_config["n_phi_cost"],
-            "ex_penalty_multiplier": run_config["ex_penalty_multiplier"],
-        }
+        rm_learner_kws=run_config["rm_learner_kws"],
     )
 
     agent_dict = {ag.agent_id: learning_ag for ag in rm_agents}
     env_dict = {f"E{i}": env for i, env in enumerate(envs)}
 
     trainer = Trainer(env_dict, env_dict, agent_dict)
-    return trainer.run(run_config)
+    result = trainer.run(run_config)
+    print(f"Result for this session was {result}")
+    return result
 
 
 if __name__ == '__main__':
