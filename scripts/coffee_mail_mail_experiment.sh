d7372740c1e9a7e845af1b46efd94fc2f8f33b39
Roko Parac <rp218@gpu20.doc.ic.ac.uk>
2024-04-03 08:06:27 +0100

    Bug fix scripts

--- scripts/coffee_mail_mail_experiment.sh
+++ scripts/coffee_mail_mail_experiment.sh
@@ -7,5 +7,5 @@ noise_levels=(0.9 0.95 0.99 1)
 for item in "${noise_levels[@]}"; do
   name="mail_error_coffee_mail_${item}"
   # run noise on all three
-  python submit_condor_script.py ${name} dqrm_coffee_world.py env/office-world@env=coffee_mail run=dqrm_coffee_world +experiment=vanilla_coffee_symmetric_error x=${item} run.name=${name}
+  python submit_condor_script.py ${name} dqrm_coffee_world.py env/office-world@env=deliver_coffee_mail run=dqrm_coffee_world +experiment=vanilla_mail_symmetric_error x=${item} run.name=${name}
 done
