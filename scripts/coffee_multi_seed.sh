df769be52fb1b721333fd9edbf2d5b109d6bff46
Roko Parac <roko.parac@gmail.com>
2024-04-07 20:33:25 +0200

    Adds coffee multi seed script

---
+++ scripts/coffee_multi_seed.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd ..
+
+seeds=(123 124 125 126 127)
+noise_levels=(1 0.99 0.95 0.9)
+
+for seed in "${seeds[@]}"; do
+  for noise_level in "${noise_levels[@]}"; do
+    name="coffee_error_${seed}_${noise_level}"
+    # run noise on all three
+    python submit_rcs_script.py ${name} dqrm_coffee_world.py env/office-world@env=deliver_coffee run=dqrm_coffee_world +experiment=vanilla_coffee_symmetric_error x=${item} run.name=${name} run.seed=${seed}
+  done
+done
+
+# Running on login.hx1.hpc.ic.ac.uk
