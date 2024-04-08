91b50f7a6d2dd6c969a544a03a491d0fff358795
Roko Parac <roko.parac@gmail.com>
2024-04-08 15:55:39 +0100

    Fix naming in new scripts

--- scripts/noise_all_three_no_restricted_observables.sh
+++ scripts/noise_all_three_no_restricted_observables.sh
@@ -7,7 +7,7 @@ noise_levels=(1 0.99 0.95 0.9)
 
 for seed in "${seeds[@]}"; do
   for noise_level in "${noise_levels[@]}"; do
-    name="symmetric_error_deliver_coffee_${seed}_${noise_level}"
+    name="difficult_symmetric_error_deliver_coffee_${seed}_${noise_level}"
     # run noise on all three
     python submit_rcs_script.py ${name} dqrm_coffee_world.py env/office-world@env=deliver_coffee run=dqrm_coffee_world +experiment=vanilla_all_symmetric_error x=${noise_level} run.name=${name} run.seed=${seed} env.use_restricted_observables=false
   done 
