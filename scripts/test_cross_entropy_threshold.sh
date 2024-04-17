8976c8de45c375010e4e2999a3aac5c0f07255a2
Roko Parac <roko.parac@gmail.com>
2024-04-17 15:06:30 +0100

    Adds test for cross entropy threshold

---
+++ scripts/test_cross_entropy_threshold.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd ..
+
+seeds=(123)
+thresholds=(0.4 0.5 0.6 0.8 1 1.2)
+
+for seed in "${seeds[@]}"; do
+  for threshold in "${thresholds[@]}"; do
+    name="cross_entropy_${threshold}"
+    # run noise on all three
+    python submit_rcs_script.py ${name} dqrm_coffee_world.py env/office-world@env=deliver_coffee run=dqrm_coffee_world +experiment=vanilla_coffee_symmetric_error x=0.8 run.name=${name} run.seed=${seed}
+  done
+done
+
+# Running on login.hx1.hpc.ic.ac.uk
