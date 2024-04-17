d22c88cc995ee19c34542c4870296c2d95370b30
Roko Parac <roko.parac@gmail.com>
2024-04-17 15:11:52 +0100

    Fix cross entropy parameter not being updated

--- scripts/test_cross_entropy_threshold.sh
+++ scripts/test_cross_entropy_threshold.sh
@@ -8,7 +8,7 @@ for seed in "${seeds[@]}"; do
   for threshold in "${thresholds[@]}"; do
     name="cross_entropy_${threshold}"
     # run noise on all three
-    python submit_rcs_script.py ${name} dqrm_coffee_world.py env/office-world@env=deliver_coffee_mail run=dqrm_coffee_world +experiment=vanilla_coffee_symmetric_error x=0.9 run.name=${name} run.seed=${seed}
+    python submit_rcs_script.py ${name} dqrm_coffee_world.py env/office-world@env=deliver_coffee_mail run=dqrm_coffee_world +experiment=vanilla_coffee_symmetric_error x=0.9 run.name=${name} run.seed=${seed} run.rm_learner_kws.cross_entropy_threshold=${threshold}
   done
 done
 
