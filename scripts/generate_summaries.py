97ca66373ba57008fa490458143beee4a12ea4e2
Roko Parac <roko.parac@gmail.com>
2024-04-22 11:57:23 +0100

    Prepares generate summaries for running experiments

--- scripts/generate_summaries.py
+++ scripts/generate_summaries.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import re
 import glob
 import time
@@ -8,8 +9,7 @@ from tbparse import SummaryReader
 from torch.utils.tensorboard import SummaryWriter
 
 # Important - should include at least every seed
-# log_dir_reg = '/home/rp218/projects/rm-marl/logs/test_parse/*'
-log_dir_reg = '/gpfs/home/rp218/rm-marl/saved_logs/*'
+# log_dir_reg = '/gpfs/home/rp218/rm-marl/saved_logs/*'
 # Match any file name that start with some text and end with _{seed}_{sensor_confidence}
 file_pattern = r"(.*)_(?P<seed>\d+)_(?P<sensor_confidence>[\d.]+)"
 
@@ -43,24 +43,26 @@ def write_tagged_results(grouped_df, tag_column_name, output_dir):
         val = grouped_df.loc[grouped_df[tag_column_name] == tag]
         for index, row in val.iterrows():
             curr_step = row["step"].iloc[0]
-            writer.add_scalar(tag, row['value']['mean'], curr_step)
+            writer.add_scalar(f"{tag}/mean", row['value']['mean'], curr_step)
 
     writer.close()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('logdirs')
-    parser.add_argument('output')
+    parser.add_argument('basedir', help="Directory where directories with experiments are stored")
+    parser.add_argument('output', default='summary')
     args = parser.parse_args()
 
-    log_dirs = glob.glob(args.logdirs)
+    log_dirs = os.listdir(args.basedir)
+
     all_df = to_dataframe(log_dirs, file_pattern)
     print("Generated pandas dataframe")
     grouped_result = aggregate_by_tag(all_df)
-    write_tagged_results(grouped_result, tag_column_name="tag", output_dir=args.output)
-    print("Results are written")
+    write_tagged_results(grouped_result, tag_column_name="tag", output_dir=f"{args.basedir}/{args.output}")
+    print("First results are written")
 
     env_grouped_results = aggregate_by_env(all_df)
-    write_tagged_results(env_grouped_results, tag_column_name="tag_without_env", output_dir="eval/summary")
-    # print(f"Results are written")
+    write_tagged_results(env_grouped_results, tag_column_name="tag_without_env",
+                         output_dir=f"{args.basedir}/{args.output}")
+    print(f"Second results are written")
