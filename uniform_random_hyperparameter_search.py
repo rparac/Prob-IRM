5a6eb126686ec710ef45a4589a43b097ba02cb95
Roko Parac <roko.parac@gmail.com>
2024-05-02 13:54:12 +0100

    Prepares longer hyperparameter search

--- uniform_random_hyperparameter_search.py
+++ uniform_random_hyperparameter_search.py
@@ -3,37 +3,40 @@ import random
 import subprocess
 import runpy
 
-# optimizer_kws_lr = [1e-1]
-# policy_train_freq = [1, 4, 8, 16, 32]
-# target_update_freq = list(range(500, 5000, 500))
-# lstm_hidden_state = [4, 8, 16, 32]
-# embedding_num_layers = [1, 2, 4]
-# embedding_layer_size = [8, 16]
-# embedding_output_size = [4]# [4, 8, 16]
-# exploration_rate_annealing_duration = [70000]
-# buffer_sizes = [2000]
+# optimizer_kws_lr = [1e-1] - did not seem too important
+optimizer_kws_lr = [1e-1, 1e-2, 1e-3, 1e-4]
+# policy_train_freq = [1, 4, 8, 16, 32] - did not seem too
+# target_update_freq = list(range(500, 5000, 500)) - ~3000 range
+# lstm_hidden_state = [4, 8, 16, 32] - smaller -> better
+# embedding_num_layers = [1, 2, 4] -> [1]
+# embedding_layer_size = [8, 16] -> [16]
+# embedding_output_size = [4] -> [4]
+exploration_rate_annealing_duration = [50000, 100000, 200000, 400000]
+# exploration_rate_annealing_duration = [70000] - seemed important; should try a lot larger
+# buffer_sizes = [2000] - did not look too important
 # use_gradient_clipping = [False]
-# rho = [0.8]
-# er_batch_size = [16]
-# er_start_size = [1000]
+# rho = [0.8] - [0.9]
+er_batch_size = [1, 4, 16, 32]
+# er_batch_size = [16] - looked useful - try different values
+# er_start_size = [1000] - 1000 is fine
 
 seeds = [123, 233, 333, 433, 533]
 
 random.seed(123)
 
-num_experiments = 1  # 20
+num_experiments = 20
 
-# for i in range(num_experiments):
-    # buffer_size = random.choice(buffer_sizes)
-    # u_gradient_clipping = random.choice(use_gradient_clipping)
-    # r = random.choice(rho)
-    # e_start_size = random.choice(er_start_size)
-    # e_batch_size = random.choice(er_batch_size)
-for i, seed in enumerate(seeds):
+for i in range(num_experiments):
+    lr = random.choice(optimizer_kws_lr)
+    expl_rate = random.choice(exploration_rate_annealing_duration)
+    batch_size = random.choice(er_batch_size)
+# for i, seed in enumerate(seeds):
     directory = "test_dqrm"
     name = f"{directory}_run_{i}"
     parameters = [
-        f"seed={seed}",
+        f"optimizer_kws.lr={lr}",
+        f"exploration_rate_annealing_duration={expl_rate}",
+        f"er_batch_size={batch_size}",
     ]
 
     subprocess.run(["python", "submit_rcs_script.py", directory, name, "test_dqrn_hydra.py", *parameters])
