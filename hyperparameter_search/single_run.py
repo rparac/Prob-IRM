9a43a160140d4678bf1099d8dbff8316b8a99138
Roko Parac <roko.parac@gmail.com>
2024-05-02 22:28:40 +0100

    Updates parameters for experimetns

--- hyperparameter_search/single_run.py
+++ hyperparameter_search/single_run.py
@@ -17,10 +17,10 @@ from rm_marl.envs.wrappers import NoisyLabelingFunctionComposer
 from rm_marl.trainer import Trainer
 
 run_config = {
-    'training': True, 'total_episodes': 2000, 'log_freq': 1, 'log_dir': 'logs', 'testing_freq': 400,
-    'greedy': True, 'synchronize': False, 'counterfactual_update': False, 'recording_freq': 400,
+    'training': True, 'total_episodes': 150000, 'log_freq': 1, 'log_dir': 'logs', 'testing_freq': 1000,
+    'greedy': True, 'synchronize': False, 'counterfactual_update': False, 'recording_freq': 1000,
     'no_display': False, 'seed': 123, 'name': 'test_dqrn', 'extra_debug_information': True,
-    'num_envs': 10, 'checkpoint_freq': 1000, 'restart_from_checkpoint': False, 'use_rs': True,
+    'num_envs': 10, 'checkpoint_freq': 10000, 'restart_from_checkpoint': False, 'use_rs': True,
     'rm_learner_kws': {'edge_cost': 2, 'n_phi_cost': 1, 'ex_penalty_multiplier': 2, 'min_penalty': 2,
                        'cross_entropy_threshold': 0.8, 'use_cross_entropy': True}, 'edge_cost': 2,
     'n_phi_cost': 2, 'ex_penalty_multiplier': 1
--- hyperparameter_search/start.py
+++ hyperparameter_search/start.py
@@ -11,7 +11,7 @@ study_name = "optuna_study_1"
 db_path = f"sqlite:////gpfs/home/rp218/rm-marl/outputs/{study_name}.db"
 study = optuna.create_study(study_name=study_name, storage=db_path, direction="minimize")
 
-num_experiments = 10
+num_experiments = 100
 for i in range(num_experiments):
     directory = "test_dqrm"
     name = f"{directory}_run_{i}"
