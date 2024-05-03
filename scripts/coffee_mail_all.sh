9c70474a6467d5b1f4c916de4825149fa4da6750
Roko Parac <roko.parac@gmail.com>
2024-05-03 17:36:48 +0100

    Adds backup scripts

---
+++ scripts/coffee_mail_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd ..
+
+seeds=(0 100 200 300 400)
+noise_levels=(1 0.9979081153869629 0.995305061340332 0.9814815521240234)
+
+directory="all_coffee_mail"
+for seed in "${seeds[@]}"; do
+  for noise_level in "${noise_levels[@]}"; do
+    name="${directory}_${seed}_${noise_level}"
+    # run noise on all three
+    python submit_rcs_script.py ${directory} ${name} \
+      dqrm_coffee_world.py env/office-world@env=delier_coffee_mail run=dqrm_coffee_world \
+        +experiment=vanilla_all_symmetric_error x=${noise_level} \
+        run.name=${directory}/${name} run.seed=${seed}
+  done
+done
+
+# Running on login.hx1.hpc.ic.ac.uk
