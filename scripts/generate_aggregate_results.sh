883cd215c0fcbc7ca390f92c72cf57be2278ffed
Roko Parac <roko.parac@gmail.com>
2024-05-06 15:43:36 +0100

    Prepare results aggreagatefirst attempt

--- scripts/generate_aggregate_results.sh
+++ scripts/generate_aggregate_results.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 cd ..
 
-base_dirs=('/gpfs/home/rp218/rm-marl/saved_logs/')
+base_dir="/rds/general/user/rp218/ephemeral/logs"
+# base_dirs=("$base_dir/all_coffee_mail" "$base_dir/all_deliver_coffee" "$base_dir/coffee_mail" "$base_dir/deliver_coffee" "$base_dir/visit_abcd_a" "$base_dir/with_rm_visit_abcd_a" "$base_dir/with_rm_visit_abcd_all")
+base_dirs=("$base_dir/coffee_mail")
 
 directory="generate_summaries"
 for base_dir in "${base_dirs[@]}"; do
   name="generate_summaries"
-  python submit_rcs_script.py ${name} ${directory} \
+  python submit_rcs_old_script.py ${name} ${directory} \
     script/generate_summaries.py ${base_dir}
 done
