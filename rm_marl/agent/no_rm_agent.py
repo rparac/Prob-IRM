8e28e3cdbb957ca3b0ea229140968c0a30d74142
Roko Parac <roko.parac@gmail.com>
2024-06-11 14:38:20 +0100

    Pass every call from no_rm_agent to default rm agent

--- rm_marl/agent/no_rm_agent.py
+++ rm_marl/agent/no_rm_agent.py
@@ -1,54 +1,23 @@
 from typing import Optional, Type
 
+from . import RewardMachineAgent
 from ..algo import QRM
 from ._base import Agent
 from ..algo import Algo
+from ..rm_transition.deterministic_rm_transitioner import DeterministicRMTransitioner
 
 
-# TODO: We can probably just have this class set up a default_rm_agent; removes duplication while making clear we are
-#  not using an RM
 class NoRMAgent(Agent):
     def __init__(
             self, agent_id: str, algo: Algo,
     ):
         super().__init__(agent_id)
 
-        self.algo = algo
-        self.reset()
+        rm_transitioner = DeterministicRMTransitioner(rm=None)
+        # Default RM agent is functionally equivalent to the default rm agent (has just one state)
+        self.agent = RewardMachineAgent.default_rm_agent(agent_id, rm_transitioner, algo)
+        self.agent.reset()
 
-    def reset(self, seed: Optional[int] = None, **kwargs):
-        self.u = 0
-        self.algo.on_env_reset()
-
-    def action(self, state, **kwargs):
-        return self.algo.action(state, self.u, **kwargs)
-
-    def learn(self, state, u, action, reward, done, next_state, next_u, **kwargs):
-        return self.algo.learn(state, u, action, reward, done, next_state, next_u, **kwargs)
-
-    def update_agent(
-            self, state, action, reward, terminated, truncated, is_positive_trace, next_state, labels, learning=True,
-            **kwargs
-    ):
-        loss = None
-        next_u = 0
-
-        if learning:
-            loss = self.learn(
-                state, self.u, action, reward, terminated or truncated, next_state, next_u, labels=labels,
-            )
-
-        self.u = next_u
-        return loss, set(), None
-
-    def project_labels(self, labels):
-        if isinstance(labels, dict):
-            return labels
-        return tuple(labels)
-
-    def set_log_folder(self, folder):
-        super().set_log_folder(folder)
-        self.algo.set_save_path(folder)
-
-    def get_current_state(self, agent_id=None):
-        return self.u
+    # Pass every call to the class to the self.agent
+    def __getattr__(self, item):
+        return getattr(self.agent, item)
