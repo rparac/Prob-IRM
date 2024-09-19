02a161702cc62aeffa9140bf106e8d6de2598309
Roko Parac <roko.parac@gmail.com>
2024-09-19 13:16:26 +0100

    Converts to rcs script format

--- ray_tests/run_template.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python RM_learning_PPO.py --enable-new-api-stack --wandb-project=prob-irm --stop-iters=25   \
-  --use-perfect-rm --wandb-run-name=single_agent_rm \
-  --wandb-key=INSERT
