4d04b1190b78666fdba8cba2a50dfb483ab9a46c
Roko Parac <roko.parac@gmail.com>
2024-10-08 11:14:05 +0100

    Prepares for capability run

--- scripts/ray_deliver_coffee.sh
+++ scripts/ray_deliver_coffee.sh
@@ -4,6 +4,9 @@
 
 cd ..
 
+ncpus=128
+ram=920 # Gb
+
 seeds=(0) # 100 200 300 400)
 noise_levels=(1) # 0.9979081153869629 0.995305061340332 0.9814815521240234)
 # wandb_name="single_agent_rm"
@@ -17,7 +20,7 @@ for seed in "${seeds[@]}"; do
   for noise_level in "${noise_levels[@]}"; do
     name="${wandb_name}"
 
-    cmd="python submit_rcs_old_script.py ${directory} ${name} ray_tests/RM_learning_PPO.py --enable-new-api-stack --wandb-project=prob-irm --stop-iters=25"
+    cmd="python submit_rcs_old_script.py ${ncpus} ${ram} ${directory} ${name} ray_tests/RM_learning_PPO.py --enable-new-api-stack --stop-iters=500"
     if [ "$wandb_name" = "single_agent_rm" ]; then
       cmd="$cmd --use-perfect-rm --wandb-run-name=single_agent_rm"
     elif [ "$wandb_name" = "single_agent_rm_learning" ]; then
@@ -27,12 +30,12 @@ for seed in "${seeds[@]}"; do
     elif [ "$wandb_name" = "10_agents_rm_learning" ]; then
       cmd="$cmd --custom-num-agents 10 --wandb-run-name=10_agents_rm_learning"
     elif [ "$wandb_name" = "single_agent_tune" ]; then
-      cmd="$cmd --use-perfect-rm --num-samples 5 --wandb-run-name=single_agent_tune"
+      cmd="$cmd --use-perfect-rm --num-samples 5000" # --wandb-run-name=single_agent_tune"
     else
       cmd=cmd
     fi
 
-    cmd="$cmd --wandb-key=7aa07ea83aacbc6521c091eb60561b8637a7f512"
+    # cmd="$cmd --wandb-key=7aa07ea83aacbc6521c091eb60561b8637a7f512"
     eval ${cmd}
   done
 done
