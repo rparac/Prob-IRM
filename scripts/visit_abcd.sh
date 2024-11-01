a75c2c00dd1c74269600a5d9ab2b906411aad34c
Roko Parac <roko.parac@gmail.com>
2024-11-01 10:04:01 +0000

    Adds new visit abcd script

--- scripts/visit_abcd.sh
+++ scripts/visit_abcd.sh
@@ -3,7 +3,8 @@ cd ..
 
 # TODO: seeds
 # seeds=(0 100 200 300 400)
-num_agents=(10)
+# num_agents=(10)
+use_rm_options=(False True)
 noise_levels=(1 0.9989626407623291 0.997668981552124 0.9907407760620117)
 
 nodes=2
@@ -11,15 +12,16 @@ ncpus=64
 ram=128
 
 directory="visit_abcd_a"
-for num_agent in "${num_agents[@]}"; do
+for use_rm in "${use_rm_options[@]}"; do
   for noise_level in "${noise_levels[@]}"; do
-    name="${directory}_${seed}_${noise_level}"
+    #name="${directory}_${seed}_${noise_level}"
+    name=${directory}_$([ "$use_rm" = True ] && echo "perfect_rm" || echo "rm_learning")_${noise_level}
     # run noise on all three
     python submit_rcs_script.py ${nodes} ${ncpus} ${ram} ${directory} ${name} \
       ray_tests/hydra_RM_learning_PPO.py env/office-world@env=visit_abcd run.name=${name} \
-        run.use_perfect_rm=True run.num_agents=${num_agent} run.should_tune=True \
-        run.num_env_runners=20 \
-        +hyperparams/with_rm=andrew \
+        run.use_perfect_rm=${use_rm} run.num_agents=10 run.should_tune=True \
+        run.num_env_runners=20 run.stop_iters=1000 \
+        +hyperparams/with_rm=less_partial_andrew \
         +experiment=vanilla_a_symmetric_error x=${noise_level} 
   done
 done
