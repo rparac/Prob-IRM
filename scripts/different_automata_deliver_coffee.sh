54471412b5b108ab12cb3e5744d990e27fe7b2a1
Roko Parac <roko.parac@gmail.com>
2024-12-02 22:37:18 +0000

    Adds seeds in different automata rm learning

--- scripts/different_automata_deliver_coffee.sh
+++ scripts/different_automata_deliver_coffee.sh
@@ -5,11 +5,10 @@
 
 cd ..
 
-# TODO: seeds
-# seeds=(0 100 200 300 400)
+seeds=(0 100 200 300 400)
 # num_agents=(10)
 # use_rm_options=(False True)
-ex_mult_options=(1) # 2 5 10 100)
+ex_mult_options=(1 2 5 10 100)
 noise_levels=(1) # 0.9979081153869629 0.995305061340332 0.9814815521240234)
 
 nodes=1
@@ -17,27 +16,29 @@ ncpus=64
 ram=128
 
 directory="different_aut_deliver_coffee"
-for ex_mult in "${ex_mult_options[@]}"; do
-  for noise_level in "${noise_levels[@]}"; do
-    #name="${directory}_${use_rm}_${noise_level}"
-    run_subdirectory=${directory}_multiplier_${ex_mult}
-    name=${run_subdirectory}_${noise_level}
+for seed in "${seed[@]}"; do
+  for ex_mult in "${ex_mult_options[@]}"; do
+    for noise_level in "${noise_levels[@]}"; do
+      #name="${directory}_${use_rm}_${noise_level}"
+      run_subdirectory=${directory}_multiplier_${ex_mult}
+      name=${run_subdirectory}_${noise_level}
 
-    # run noise on all three
-    # python submit_rcs_script.py ${nodes} ${ncpus} ${ram} ${directory} ${name} ray_tests/simple_test.py
-    python submit_rcs_old_script.py ${nodes} ${ncpus} ${ram} ${directory} ${name} \
-      ray_tests/hydra_RM_learning_PPO.py run.name=${name} run.seed=123 \
-        rm_learner.ex_penalty_multiplier=${ex_mult} \
-        rm_learner.min_penalty=${ex_mult} \
-        run.use_perfect_rm=False run.num_agents=1 run.should_tune=True \
-	run.tune_config.num_samples=1 \
-	run.num_env_runners=20 \
-	run.wandb.key=680ad332869d9761ae2b6bdd70cdbc068674d47b \
-	run.render_freq=20 \
-        +hyperparams/with_rm=config5 \
-        +experiment=vanilla_coffee_symmetric_error x=${noise_level}
-    # python submit_rcs_script.py ${nodes} ${ncpus} ${ram} ${directory} ${name} \
-    # ray_tests/hydra_RM_learning_PPO.py run.name=${name} run.use_perfect_rm=True run.num_agents=${num_agent} run.should_tune=True run.num_env_runners=40 run.tune_config.num_samples=200
+      # run noise on all three
+      # python submit_rcs_script.py ${nodes} ${ncpus} ${ram} ${directory} ${name} ray_tests/simple_test.py
+      python submit_rcs_old_script.py ${nodes} ${ncpus} ${ram} ${directory} ${name} \
+        ray_tests/hydra_RM_learning_PPO.py run.name=${name} run.seed=${seed} \
+          rm_learner.ex_penalty_multiplier=${ex_mult} \
+          rm_learner.min_penalty=${ex_mult} \
+          run.use_perfect_rm=False run.num_agents=1 run.should_tune=True \
+    run.tune_config.num_samples=1 \
+    run.num_env_runners=20 \
+    run.wandb.key=680ad332869d9761ae2b6bdd70cdbc068674d47b \
+    run.render_freq=20 \
+          +hyperparams/with_rm=config5 \
+          +experiment=vanilla_coffee_symmetric_error x=${noise_level}
+      # python submit_rcs_script.py ${nodes} ${ncpus} ${ram} ${directory} ${name} \
+      # ray_tests/hydra_RM_learning_PPO.py run.name=${name} run.use_perfect_rm=True run.num_agents=${num_agent} run.should_tune=True run.num_env_runners=40 run.tune_config.num_samples=200
+    done
   done
 done
 
