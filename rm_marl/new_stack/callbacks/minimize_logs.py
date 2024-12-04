45800ce956270296c95d129a27324a031b666414
Lorenzo Nodari <28101148+lorenzonodari@users.noreply.github.com>
2024-12-04 15:52:24 +0100

    Refine the amount of logged data when run.minimize_logs=True

--- rm_marl/new_stack/callbacks/minimize_logs.py
+++ rm_marl/new_stack/callbacks/minimize_logs.py
@@ -25,9 +25,9 @@ class MinimizeLogs(DefaultCallbacks):
         del result['fault_tolerance']
         del result['perf']
         del result['learners']
-        del result['iterations_since_restore']
+        # del result['iterations_since_restore'] -> Might be used when checkpointing
         del result['time_this_iter_s']
-        del result['time_since_restore']
+        # del result['time_since_restore'] -> Might be used when checkpointing
         del result['timestamp']
         del result['num_episodes_lifetime']
         # del result['num_env_steps_trained_lifetime'] -> Used by the CLIReporter
@@ -37,17 +37,19 @@ class MinimizeLogs(DefaultCallbacks):
         if 'evaluation' in result:
             del result['evaluation']
 
-        del result['env_runners']['agent_steps']
         del result['env_runners']['episode_len_min']
         del result['env_runners']['episode_len_max']
         del result['env_runners']['episode_len_mean']
         del result['env_runners']['episode_duration_sec_mean']
-        del result['env_runners']['num_env_steps_sampled']
-        del result['env_runners']['num_agent_steps_sampled']
-        del result['env_runners']['num_module_steps_sampled']
-        del result['env_runners']['num_env_steps_sampled_lifetime']
-        del result['env_runners']['num_module_steps_sampled_lifetime']
-        del result['env_runners']['num_agent_steps_sampled_lifetime']
 
+        # del result['env_runners']['agent_steps']
+        # del result['env_runners']['num_env_steps_sampled']
+        # del result['env_runners']['num_agent_steps_sampled']
+        del result['env_runners']['num_module_steps_sampled']
+
+        # del result['env_runners']['num_env_steps_sampled_lifetime']
+        # del result['env_runners']['num_agent_steps_sampled_lifetime']
+        del result['env_runners']['num_module_steps_sampled_lifetime']
+
+        # del result['env_runners']['agent_episode_returns_mean']
         del result['env_runners']['module_episode_returns_mean']
-        del result['env_runners']['agent_episode_returns_mean']
