80fe1ad66b4d6d72a35e43c5923c450804380c3f
Roko Parac <roko.parac@gmail.com>
2024-12-05 10:57:38 +0000

    Fix rm learning visit abcd script

--- scripts/rm_learning_visit_abcd.sh
+++ scripts/rm_learning_visit_abcd.sh
@@ -22,7 +22,7 @@ for seed in "${seeds[@]}"; do
           rm_learner.ex_penalty_multiplier=8 \
           rm_learner.min_penalty=8 \
           env.max_episode_length=1000 \
-	  run.crash_iters=501 \
+	  run.crash_iter=501 \
 	  run.tune_config.checkpoint_freq=100 \
           run.use_perfect_rm=${use_rm} run.num_agents=10 run.should_tune=True \
   	      run.tune_config.num_samples=1 \
